46742a20cf72babafc86a639b744b437f649aa4d
佑子 <shizhiqi.9805@bytedance.com>
2022-09-16 17:40:04 +0800

    first commit

---
+++ 1.string.ts
@@ -0,0 +1,4 @@
+const str: string = 1234;
+let str1: string = '1234';
+str1 = '2345';
+str1 = 1234;
---
+++ 2.number.ts
@@ -0,0 +1,4 @@
+const num: number = '1234';
+let num1: number = 1234;
+num1 = '2345';
+num1 = 1234;
